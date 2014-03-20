b0fdd18d01aa5afa7d0952b7230e7ef125ce221e
thunder <thunder@debianserver.thunderace.fr>
2014-03-20 10:04:31 +0100

    example added

---
+++ example/simple.js
@@ -0,0 +1,36 @@
+var newtifryPro = require('../lib/NewtifryPro');
+var fs = require('fs');
+
+var apikey = 'Your APIKEY';
+var registrationIds = [];
+registrationIds.push('You first device ID');
+registrationIds.push('You second device ID');
+
+var message = {
+	collapseKey: 'demo',	// optional
+	delayWhileIdle: true,	// optional
+	timeToLive: 3,			// optional
+	data: {
+		type: 'ntp_message',
+		message: new Buffer('Test message body').toString('base64'),
+		priority:	2,
+		title: new Buffer('Test message').toString('base64'),
+		source: new Buffer('domo2').toString('base64'),
+//		url: '', // new Buffer('Test message').toString('base64'),
+//		image: '', // new Buffer('Test message').toString('base64'),
+//		speak: 1,
+//		nocache: 0
+    }
+};
+
+
+var now = new Date();
+message.data.timestamp = now.toISOString();
+
+newtifryPro.sendMessage(message, apikey, registrationIds, function (err, data) {
+	if (err === null) {
+		console.log(data);
+	} else {
+		console.log(err);
+	}
+});

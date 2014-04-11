76cf3f37a989f4ff63259d114fa2f73fa1019323
thunder <thunder@debianserver.thunderace.fr>
2014-04-11 09:26:19 +0200

    update

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Nodejs npm library to send messages to android app NewtifryPro
 
 Not yet npm packaged.
 
-To install : https://github.com/thunderace/NewtifryPro-node
+To install : npm install git://github.com/thunderace/NewtifryPro-node.git                                                                                                                                                                                    
+
 
---
+++ example/OOAPI.js
@@ -0,0 +1,21 @@
+var NewtifryProMessage = require('../lib/NewtifryPro').NewtifryProMessage;
+
+var apikey = 'Your APIKEY';
+var registrationId = 'You first device ID';
+
+
+var message1 = new NewtifryProMessage();
+
+message1.setTitle('test message 1');
+message1.setSenderId(apikey);
+message1.addRegistrationId(registrationId);
+message1.setMessage('OOAPI test');
+message1.setSticky();
+message1.send(function (err, data) {
+	if (err === null) {
+		console.log(data);
+	} else {
+		counter--;
+		console.log(err);
+	}
+});
